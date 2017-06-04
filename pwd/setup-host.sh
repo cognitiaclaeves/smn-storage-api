416bd702d4b190331c5ac4fd163f198363e90ba7
cognetizen <cognetizen@gmail.com>
2017-06-04 00:39:43 +0000

    ..

--- pwd/setup-host.sh
+++ pwd/setup-host.sh
@@ -3,7 +3,7 @@ apk update
 apk add ansible
 
 # for now .. in future, maybe add this to role?
-git config --global user.email 'cognetizen@gmail.com'
-git config --global user.name 'Cognetizen'
+# git config --global user.email 'cognetizen@gmail.com'
+# git config --global user.name 'Cognetizen'
 
 
