4c0a013bad98d616b2a905988607a7e2097e9bbe
cognetizen <cognetizen@gmail.com>
2017-06-04 01:44:17 +0000

    .

--- pwd/setup-host.sh
+++ pwd/setup-host.sh
@@ -6,4 +6,7 @@ apk add ansible
 # git config --global user.email 'cognetizen@gmail.com'
 # git config --global user.name 'Cognetizen'
 
+# Move current directory to -https .. 
+# WARNING: Don't run this file more than once.
+mv ~/smn-storage-api ~/smn-storage-api-https 2>/dev/null
 
